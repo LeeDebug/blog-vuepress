8d5b92d83fd792648187dc2c4491b223e09ed81f
李淳淳 <961150665@qq.com>
2021-11-05 16:56:43 +0800

    启动时，修改启动域名和端口号

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,4 +1,10 @@
+const personalDev = require('./config/env/personal.config').getConfig()
+
 module.exports = {
+  // 启动域名
+  host: personalDev.host,
+  // 启动端口
+  port: personalDev.port,
   // 是否需要缓存
   // TODO By Licc: 为何写在 npm script 里有效，写在这里却无效？
   // cache: 'blog/.vuepress/.cache',
